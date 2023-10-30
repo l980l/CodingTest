9846e18ef079f6862d10b25c09dc030b42de3d9d
l980l <kimkwanhyeon98@gmail.com>
2023-10-30 15:58:48 +0900

    BOJ 1021 회전하는 큐

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -8,83 +8,64 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int N;
-	cin >> N;
+	int N, M;
+	cin >> N >> M;
+	
+	int Sum = 0;
 
+	// 덱에 1~N 넣기.
 	deque<int> Deque;
-
-	while (N--)
+	for (int i = 1; i <= N; ++i)
 	{
-		string S;
-		cin >> S;
+		Deque.push_back(i);
+	}
 
-		if (S == "push_front")
+	while (M--)
+	{
+		int a;
+		cin >> a;
+
+		int Idx = 0;
+		while (!Deque.empty())
 		{
-			int a;
-			cin >> a;
-
-			Deque.push_front(a);
-		}
-
-		else if (S == "push_back")
-		{
-			int a;
-			cin >> a;
-
-			Deque.push_back(a);
-		}
-
-		else if (S == "pop_front")
-		{
-			if (Deque.empty())
-				cout << -1 << "\n";
-			else
+			if (Deque.at(Idx) == a)
 			{
-				cout << Deque.front() << "\n";
+				Sum += Idx;
+
+				// 맨 앞으로 옮기고
+				while (Idx--)
+				{
+					Deque.push_back(Deque.front());
+					Deque.pop_front();
+				}
+
+				// 맨 앞 팝.
 				Deque.pop_front();
+				break;
 			}
-		}
 
-		else if (S == "pop_back")
-		{
-			if (Deque.empty())
-				cout << -1 << "\n";
-			else
+			else if (Deque.at(Deque.size() - 1 - Idx) == a)
 			{
-				cout << Deque.back() << "\n";
-				Deque.pop_back();
+				++Idx;
+				Sum += Idx;
+
+				// 맨 앞으로 옮기고
+				while (Idx--)
+				{
+					Deque.push_front(Deque.back());
+					Deque.pop_back();
+				}
+
+				// 맨 앞 팝.
+				Deque.pop_front();
+				break;
 			}
-		}
 
-		else if (S == "size")
-		{
-			cout << Deque.size() << "\n";
-		}
-
-		else if (S == "empty")
-		{
-			if (Deque.empty())
-				cout << 1 << "\n";
-			else
-				cout << 0 << "\n";
-		}
-
-		else if (S == "front")
-		{
-			if (Deque.empty())
-				cout << -1 << "\n";
-			else
-				cout << Deque.front() << "\n";
-		}
-
-		else if (S == "back")
-		{
-			if (Deque.empty())
-				cout << -1 << "\n";
-			else
-				cout << Deque.back() << "\n";
+			Idx++;
 		}
 	}
+
+	cout << Sum;
 
 	return 0;
 }
