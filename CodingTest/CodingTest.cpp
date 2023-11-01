5938f39632eaf8c4ed13a5d2a07d7ac09daa3cd9
l980l <kimkwanhyeon98@gmail.com>
2023-11-01 22:02:57 +0900

    BOJ 2178 미로 탐색

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,6 +1,6 @@
-﻿#include <iostream>
-#include <queue>
+﻿#include <iostream>	
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -9,70 +9,52 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n, m;
-	cin >> n >> m;
+	int N, M;
+	cin >> N >> M;
 
-	int PaintCount = 0;
-	int max = 0;
+	vector<vector<int>> Board(N, vector<int>(M));
+	vector<vector<int>> Distance(N, vector<int>(M));
 
-	int dx[4] = { 1, 0, -1, 0 };
-	int dy[4] = { 0, 1, 0, -1 };
+	int dx[4] = { 0, 1, 0, -1 };
+	int dy[4] = { 1, 0, -1, 0 };
 
-	vector<vector<int>> Board(n, vector<int>(m));
-	vector<vector<bool>> Visit(n, vector<bool>(m));
+	for (int i = 0; i < N; ++i)
+	{
+		string S;
+		cin >> S;
+
+		for (int j = 0; j < M; ++j)
+		{
+			Distance[i][j] = -1;
+			Board[i][j] = S[j] - '0';
+		}
+	}
+
 	queue<pair<int, int>> Queue;
 
-	for (int i = 0; i < n; ++i)
+	Queue.push({ 0,0 });
+	Distance[0][0] = 1;
+
+	while (!Queue.empty())
 	{
-		for (int j = 0; j < m; ++j)
+		pair<int, int> Pair = Queue.front();
+		Queue.pop();
+
+		for (int i = 0; i < 4; ++i)
 		{
-			int a;
-			cin >> a;
-			Board[i][j] = a;
+			int nx = Pair.first + dx[i];
+			int ny = Pair.second + dy[i];
+
+			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+				continue;
+			if (!Board[nx][ny] || Distance[nx][ny] != -1)
+				continue;
+			Queue.push({ nx, ny });
+			Distance[nx][ny] = Distance[Pair.first][Pair.second] + 1;
 		}
 	}
 
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			if (!Visit[i][j] && Board[i][j])
-			{
-				// 그림 개수 늘리기.
-				PaintCount++;
-
-				Visit[i][j] = true;
-				Queue.push({ i,j });
-
-				int temp = 1;
-
-				while (!Queue.empty())
-				{
-					pair<int, int> Pair = Queue.front();
-					Queue.pop();
-
-					for (int i = 0; i < 4; ++i)
-					{
-						if (Pair.first + dx[i] < 0 || Pair.first + dx[i] >= n || Pair.second + dy[i] < 0 || Pair.second + dy[i] >= m)
-							continue;
-
-						else if (Board[Pair.first + dx[i]][Pair.second + dy[i]] && !Visit[Pair.first + dx[i]][Pair.second + dy[i]])
-						{
-							Queue.push({ Pair.first + dx[i], Pair.second + dy[i] });
-							Visit[Pair.first + dx[i]][Pair.second + dy[i]] = true;
-							temp++;
-						}
-					}
-				}
-
-				if (max < temp)
-					max = temp;
-			}
-		}
-	}
-
-	cout << PaintCount << "\n";
-	cout << max << "\n";
+	cout << Distance[N - 1][M - 1];
 
 	return 0;
 }
