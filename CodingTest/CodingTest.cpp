f7a17c271a98698503fe1878d1ec6419041b8277
l980l <kimkwanhyeon98@gmail.com>
2023-11-02 19:15:16 +0900

    BOJ 7562 나이트의 이동

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,7 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
 #include <queue>
-#include <tuple>
 
 using namespace std;
 
@@ -10,93 +9,51 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int M, N, H;
-	cin >> M >> N >> H;
-	vector<vector<vector<int>>> Board(N, vector<vector<int>>(M, vector<int>(H)));
-	vector<vector<vector<int>>> Distance(N, vector<vector<int>>(M, vector<int>(H)));
-	int dx[4] = { 0,-1,0,1 };
-	int dy[4] = { 1,0,-1,0 };
-	int dz[2] = { 1,-1 };
-	queue <tuple<int, int, int>> Queue;
+	int dx[8] = { 1,2,2,1,-1,-2,-2, -1 };
+	int dy[8] = { -2,-1,1,2,2,1,-1,-2 };
 
-	for (int i = 0; i < H; ++i)
+	int T;
+	cin >> T;
+	while (T--)
 	{
-		for (int j = 0; j < N; ++j)
+		int n;
+		cin >> n;
+		vector<vector<int>> Distance(n, vector<int>(n));
+		queue<pair<int, int>> Queue;
+		for (int i = 0; i < n; ++i)
 		{
-			for (int h = 0; h < M; ++h)
+			for (int j = 0; j < n; ++j)
 			{
-				int a;
-				cin >> a;
-				Board[j][h][i] = a;
-				// 초기부터 익은 토마토는 queue에 넣고 시작하자. 
-				if (a == 1)
-				{
-					Queue.push({ j,h,i });	// tuple 헤더를 인클루드하니까 쓸 수 있음.
-					Distance[j][h][i] = 0;
-				}
-				else
-					Distance[j][h][i] = -1;
+				Distance[i][j] = -1;
 			}
 		}
-	}
-
-	while (!Queue.empty())
-	{
-		int r, c, h;
-		tie(r, c, h) = Queue.front();
-		Queue.pop();
-
-		// dx, dy
-		for (int i = 0; i < 4; ++i)
+		// 나이트의 초기 위치
+		int x, y;
+		cin >> x >> y;
+		Distance[x][y] = 0;
+		Queue.push({ x,y });
+		while (!Queue.empty())
 		{
-			int nx = r + dx[i];
-			int ny = c + dy[i];
+			auto Pair = Queue.front();
+			Queue.pop();
 
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-				continue;
-			// 익지 않은 토마토인지만 검사해도 방문 여부도 판단할 수 있다. 
-			if (Board[nx][ny][h] != 0)
-				continue;
-			Distance[nx][ny][h] = Distance[r][c][h] + 1;
-			Board[nx][ny][h] = 1;
-			Queue.push({ nx,ny,h });
-		}
-		// dz
-		for (int i = 0; i < 2; ++i)
-		{
-			int nz = h + dz[i];
-
-			if (nz < 0 || nz >= H)
-				continue;
-			// 익지 않은 토마토인지만 검사해도 방문 여부도 판단할 수 있다. 
-			if (Board[r][c][nz] != 0)
-				continue;
-			Distance[r][c][nz] = Distance[r][c][h] + 1;
-			Board[r][c][nz] = 1;
-			Queue.push({ r,c,nz });
-		}
-	}
-
-	int max = 0;
-	for (int i = 0; i < H; ++i)
-	{
-		for (int j = 0; j < N; ++j)
-		{
-			for (int h = 0; h < M; ++h)
+			for (int i = 0; i < 8; ++i)
 			{
-				if (Board[j][h][i] == 0)
-				{
-					cout << -1;
-					return 0;
-				}
+				int nx = Pair.first + dx[i];
+				int ny = Pair.second + dy[i];
 
-				if (Distance[j][h][i] > max)
-					max = Distance[j][h][i];
+				if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+					continue;
+				if (Distance[nx][ny] != -1)
+					continue;
+				Distance[nx][ny] = Distance[Pair.first][Pair.second] + 1;
+				Queue.push({ nx,ny });
 			}
 		}
+		// 나이트의 목적 위치
+		cin >> x >> y;
+		cout << Distance[x][y] << "\n";
 	}
-
-	cout << max;
 
 	return 0;
 }
