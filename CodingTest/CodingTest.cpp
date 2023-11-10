422c4a626c6f667da1e04261545dc231c1957630
l980l <kimkwanhyeon98@gmail.com>
2023-11-10 11:27:44 +0900

    BOJ 1074 Z

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,69 +1,29 @@
 ﻿#include <iostream>
-#include <queue>
-#include <vector>
 
 using namespace std;
 
-int N, K;
-int Dist[100003];
-queue<int> Queue;
-
-void teleport(int S)
+int func(int n, int r, int c)
 {
-	if (S == 0)
-		return;
-	int temp = S;
-
-	temp <<= 1;
-	while (temp - 1 <= K)
-	{
-		if (Dist[temp] == -1)
-		{
-			Dist[temp] = Dist[S];
-			Queue.push(temp);
-			if (temp == K)
-				return;
-		}
-		temp <<= 1;
-	}
+	if (n == 0)
+		return 0;
+	int half = 1 << (n - 1);
+	if (r < half && c < half)
+		return func(n - 1, r, c);
+	if (r < half && c >= half)
+		return half * half + func(n - 1, r, c - half);
+	if (r >= half && c < half)
+		return 2 * half * half + func(n - 1, r - half, c);
+	return 3 * half * half + func(n - 1, r - half, c - half);
 }
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-	cin >> N >> K;
-
-	fill(Dist, Dist + 100003, -1);
-	Dist[N] = 0;
-	Queue.push(N);
-	teleport(N);
-	while (!Queue.empty())
-	{
-		int Cur = Queue.front();
-		Queue.pop();
-
-		if (Cur == K)
-		{
-			cout << Dist[Cur];
-			return 0;
-		}
-
-		if (Cur - 1 >= 0 && Dist[Cur - 1] == -1)
-		{
-			Dist[Cur - 1] = Dist[Cur] + 1;
-			Queue.push(Cur - 1);
-			teleport(Cur - 1);
-		}
-
-		if (Cur + 1 <= 100001 && Dist[Cur + 1] == -1)
-		{
-			Dist[Cur + 1] = Dist[Cur] + 1;
-			Queue.push(Cur + 1);
-			teleport(Cur + 1);
-		}
-	}
+	
+	int n, r, c;
+	cin >> n >> r >> c;
+	cout << func(n, r, c);
 
 	return 0;
 }
