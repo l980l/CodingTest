8442a263ce10928b68b5e59d57ba7a7b9fe1a796
l980l <kimkwanhyeon98@gmail.com>
2023-11-10 18:22:09 +0900

    BOJ 1992 쿼드트리

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,8 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
 using namespace std;
-int Blue;
-int White;
 vector<vector<int>> Board;
 
 void func(int n, int sr, int sc)
@@ -13,23 +11,22 @@ void func(int n, int sr, int sc)
     {
         for (int j = sc; j < sc + n; ++j)
         {
-            if (Board[i][j] != temp)
+            if (temp != Board[i][j])
                 OneColor = false;
         }
     }
     if (OneColor)
     {
-        if (temp)
-            Blue++;
-        else
-            White++;
+        cout << temp-'0';
     }
     else
     {
+        cout << "(";
         func(n / 2, sr, sc);
         func(n / 2, sr, sc+n/2);
         func(n / 2, sr+n/2, sc);
         func(n / 2, sr+n/2, sc+n/2);
+        cout << ")";
     }
 }
 
@@ -38,22 +35,20 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    Blue = 0;
-    White = 0;
     int N;
     cin >> N;
     Board = vector<vector<int>>(N, vector<int>(N));
     for (int i = 0; i < N; ++i)
     {
+        string S;
+        cin >> S;
         for (int j = 0; j < N; ++j)
         {
-            cin >> Board[i][j];
+            Board[i][j] = S[j];
         }
     }
-    func(N, 0, 0);
 
-    cout << White << '\n';
-    cout << Blue << '\n';
+    func(N, 0, 0);
 
     return 0;
 }
