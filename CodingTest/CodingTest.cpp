1394d6f82244c2f78eacbef280a8bc1efa794c51
l980l <kimkwanhyeon98@gmail.com>
2023-11-10 16:20:58 +0900

    BOJ 17478 재귀함수가 뭔가요?

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -2,18 +2,48 @@
 
 using namespace std;
 
-int func(int n, int r, int c)
+void func(int n, int now)
 {
-	if (n == 0)
-		return 0;
-	int half = 1 << (n - 1);
-	if (r < half && c < half)
-		return func(n - 1, r, c);
-	if (r < half && c >= half)
-		return half * half + func(n - 1, r, c - half);
-	if (r >= half && c < half)
-		return 2 * half * half + func(n - 1, r - half, c);
-	return 3 * half * half + func(n - 1, r - half, c - half);
+	for (int i = 0; i < now; ++i)
+	{
+		cout << "____";
+	}
+	cout << '"' << "재귀함수가 뭔가요?" << '"' << '\n';
+	
+	if (now == n)
+	{
+		for (int i = 0; i < now; ++i)
+		{
+			cout << "____";
+		}
+		cout << '"' << "재귀함수는 자기 자신을 호출하는 함수라네" << '"' << '\n';
+	}
+
+	else
+	{
+		for (int i = 0; i < now; ++i)
+		{
+			cout << "____";
+		}
+		cout << '"' << "잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
+		for (int i = 0; i < now; ++i)
+		{
+			cout << "____";
+		}
+		cout << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
+		for (int i = 0; i < now; ++i)
+		{
+			cout << "____";
+		}
+		cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어." << '"' << '\n';
+		func(n, now + 1);
+	}
+
+	for (int i = 0; i < now; ++i)
+	{
+		cout << "____";
+	}
+	cout << "라고 답변하였지.\n";
 }
 
 int main()
@@ -21,9 +51,10 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
-	int n, r, c;
-	cin >> n >> r >> c;
-	cout << func(n, r, c);
+	int n;
+	cin >> n;
+	cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n";
+	func(n, 0);
 
 	return 0;
 }
