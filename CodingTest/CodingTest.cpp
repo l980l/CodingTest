beff05ca0753e889b2bb89e0960dc958a38dfcb6
l980l <k910208@gmail.com>
2023-11-18 18:41:16 +0900

    1600 말이 되고 싶은 원숭이

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,69 +1,95 @@
 ﻿#include <iostream>
 #include <queue>
 #include <vector>
+#include <tuple>
 
 using namespace std;
 
-int N, K;
-int Dist[100003];
-queue<int> Queue;
+// 말 이동
+int hdx[8] = { -2,-1,1,2,-2,-1,1,2 };
+int hdy[8] = { 1,2,2,1,-1,-2,-2,-1 };
+// 원숭이 이동
+int mdx[4] = { 0,0,1,-1 };
+int mdy[4] = { 1,-1,0,0 };
 
-void teleport(int S)
-{
-	if (S == 0)
-		return;
-	int temp = S;
+int K, W, H;
 
-	temp <<= 1;
-	while (temp - 1 <= K)
-	{
-		if (Dist[temp] == -1)
-		{
-			Dist[temp] = Dist[S];
-			Queue.push(temp);
-			if (temp == K)
-				return;
-		}
-		temp <<= 1;
-	}
-}
+vector<vector<int>> Board;
+vector<vector<vector<int>>> Distance;
+queue<tuple<int, int, int>> Queue;	// 3번째 요소는 남은 K 수임.
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	
+	cin >> K >> W >> H;
+	Board = vector<vector<int>>(H, vector<int>(W));
+	Distance = vector<vector<vector<int>>>(31, vector<vector<int>>(H, vector<int>(W)));
 
-	cin >> N >> K;
-
-	fill(Dist, Dist + 100003, -1);
-	Dist[N] = 0;
-	Queue.push(N);
-	teleport(N);
-	while (!Queue.empty())
+	for (int i = 0; i < H; ++i)
 	{
-		int Cur = Queue.front();
-		Queue.pop();
-
-		if (Cur == K)
+		for (int j = 0; j < W; ++j)
 		{
-			cout << Dist[Cur];
-			return 0;
-		}
-
-		if (Cur - 1 >= 0 && Dist[Cur - 1] == -1)
-		{
-			Dist[Cur - 1] = Dist[Cur] + 1;
-			Queue.push(Cur - 1);
-			teleport(Cur - 1);
-		}
-
-		if (Cur + 1 <= 100001 && Dist[Cur + 1] == -1)
-		{
-			Dist[Cur + 1] = Dist[Cur] + 1;
-			Queue.push(Cur + 1);
-			teleport(Cur + 1);
+			cin >> Board[i][j];
+			for (int w = 0; w < 31; ++w)
+			{
+				Distance[w][i][j] = -1;
+			}
 		}
 	}
+
+	Distance[0][0][0] = 0;
+	Queue.push({ 0, 0, K });
+	while (!Queue.empty())
+	{
+		int r, c, k;
+		tie(r, c, k) = Queue.front();
+		Queue.pop();
+
+		// 말로 이동할 때
+		if (k)
+		{
+			for (int i = 0; i < 8; ++i)
+			{
+				int nx = r + hdx[i];
+				int ny = c + hdy[i];
+
+				if (nx < 0 || nx >= H || ny < 0 || ny >= W)
+					continue;
+				if (Board[nx][ny] == 1 || Distance[K - k + 1][nx][ny] != -1)
+					continue;
+				Distance[K - k + 1][nx][ny] = Distance[K - k][r][c] + 1;
+				Queue.push({ nx,ny, k - 1 });
+			}
+		}
+		// 원숭이로 이동할 때
+		for (int i = 0; i < 4; ++i)
+		{
+			int nx = r + mdx[i];
+			int ny = c + mdy[i];
+
+			if (nx < 0 || nx >= H || ny < 0 || ny >= W)
+				continue;
+			if (Board[nx][ny] == 1 || Distance[K - k][nx][ny] != -1)
+				continue;
+			Distance[K-k][nx][ny] = Distance[K - k][r][c] + 1;
+			Queue.push({ nx,ny, k });
+		}
+	}
+	// 결과 도출
+	int  Result = -1;
+	for (int i = 0; i < 31; ++i)
+	{
+		if (Distance[i][H - 1][W - 1] != -1)
+		{
+			if (Result == -1)
+				Result = Distance[i][H - 1][W - 1];
+			Result = min(Result, Distance[i][H - 1][W - 1]);
+		}
+	}
+
+	cout << Result;
 
 	return 0;
 }
