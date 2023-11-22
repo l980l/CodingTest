02de2e61c2ac5cf5f48cd99db2a3d2b85660c7d5
l980l <k910208@gmail.com>
2023-11-22 21:54:39 +0900

    BOJ 15650 N과 M (2)

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -2,45 +2,41 @@
 
 using namespace std;
 
-int N, S;
-int arr[20];
-int result = 0;
+int N, M;
+int arr[9];
+bool isused[9];
 
-void func(int curIdx, int sum);
+void func(int count, int least)
+{
+	if (count == M)
+	{
+		for (int i = 0; i < M; ++i)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = least; i <= N; ++i)
+	{
+		if (!isused[i])
+		{
+			arr[count] = i;
+			isused[i] = true;
+			func(count + 1, i);
+			isused[i] = false;
+		}
+	}
+}
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-	cin >> N >> S;
-
-	for (int i = 0; i < N; ++i)
-	{
-		cin >> arr[i];
-	}
 	
-	func(0, 0);
-	
-	// S가 0인 경우에는 부분 수열이 공집합인 경우를 제외해야 하기 때문에 -1을 해준다. 
-	if (S == 0)
-		result--;
-
-	cout << result;
+	cin >> N >> M;
+	func(0, 1);
 
 	return 0;
-}
-
-void func(int curIdx, int sum)
-{
-	if (curIdx == N)
-	{
-		if (sum == S)
-			result++;
-		return;
-	}
-	// 현재 값을 더하지 않는 경우
-	func(curIdx + 1, sum);
-	// 현재 값을 더한 경우
-	func(curIdx + 1, sum + arr[curIdx]);
 }
