3155d39d5bc332f1ddd586c0840ee5fd499d0a0e
l980l <kimkwanhyeon98@gmail.com>
2024-03-07 20:56:26 +0900

    BOJ 18808 스티커 붙이기

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,32 +1,62 @@
 ﻿#include <iostream>	
-#include <vector>	
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-int N, M;
-int Min;
-vector<vector<int>> originboard;
-vector<vector<int>> copyboard;
-vector<pair<int,int>> cctv;
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
+int N, M, K;
+vector<vector<bool>> board;
+queue<vector<vector<bool>>> stickers;
 
-void look(int x, int y, int dir)
+// 0번 스티커 Turn 시키기.
+void TurnSticker(vector<vector<bool>>& sticker)
 {
-	dir %= 4;
-
-	while (true)
+	int SizeX = sticker.size();	// 열 길이
+	int SizeY = sticker.front().size();	// 어차피 직사각형이라 모든 행의 길이가 같음. 
+	// 새로운 스티커
+	vector<vector<bool>> NewSticker;
+	for (int i = 0; i < SizeY; ++i)
 	{
-		x += dx[dir];
-		y += dy[dir];
-
-		if (x < 0 || x >= N || y < 0 || y >= M)
-			break;
-		if (copyboard[x][y] == 6)
-			break;
-		if (copyboard[x][y] == 0)
-			copyboard[x][y] = '#';
+		vector<bool> temp1;
+		for (int j = SizeX -1; j >= 0; --j)
+		{
+			temp1.push_back(sticker[j][i]);
+		}
+		NewSticker.push_back(temp1);
 	}
+	// 새로 만든 스티커로 바꾸기
+	sticker = NewSticker;
+}
+
+// 스티커의 LeftTop 좌표 값. 
+bool Stick(int x, int y, const vector<vector<bool>>& sticker)
+{
+	int SizeX = sticker.size();	// 열 길이
+	int SizeY = sticker.front().size();	// 어차피 직사각형이라 모든 행의 길이가 같음. 
+	for (int i = 0; i < SizeX; ++i)
+	{
+		for (int j = 0; j < SizeY; ++j)
+		{
+			int nx = x + i;
+			int ny = y + j;
+			// 노트북 넘어감. 
+			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+				return false;
+			// 붙일 수 없음.
+			if (board[nx][ny] == 1 && sticker[i][j] == 1)
+				return false;
+		}
+	}
+	// 붙일 수 있는 경우
+	for (int i = 0; i < SizeX; ++i)
+	{
+		for (int j = 0; j < SizeY; ++j)
+		{
+			if (board[x + i][y + j] == 0 && sticker[i][j] == 1)
+				board[x + i][y + j] = 1;
+		}
+	}
+	return true;
 }
 
 int main()
@@ -34,88 +64,71 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> N >> M;
+	cin >> N >> M >> K;
+	board = vector<vector<bool>>(N, vector<bool>(M));
 
-	originboard = vector<vector<int>>(N, vector<int>(M));
+	for (int i = 0; i < K; ++i)
+	{
+		int x, y;
+		cin >> x >> y;
+		vector<vector<bool>> temp1;
+		for (int c = 0; c < x; ++c)
+		{
+			vector<bool> temp2;
+			for (int r = 0; r < y; ++r)
+			{
+				bool input;
+				cin >> input;
+				temp2.push_back(input);
+			}
+			temp1.push_back(temp2);
+		}
+		stickers.push(temp1);
+	}
 
+	while (!stickers.empty())
+	{
+		vector<vector<bool>> sticker = stickers.front();
+		stickers.pop();
+		bool isSticked = false;
+		int TurnCount = 0;
+
+		// 붙였거나, 4번 회전시켰거나.
+		while (!isSticked && TurnCount < 4)
+		{
+			for (int i = 0; i < N; ++i)
+			{
+				for (int j = 0; j < M; ++j)
+				{
+					// 스티커의 크기를 고려해서 LT의 위치로 가능한 위치를 확인해본다.
+					if (Stick(i, j, sticker))
+					{
+						// 스티커 붙이기 성공.
+						isSticked = true;
+						break;
+					}
+				}
+				if (isSticked)
+					break;
+			}
+			if (isSticked)
+				break;
+			// 회전시키기.
+			TurnSticker(sticker);
+			TurnCount++;
+		}
+	}
+
+	int result = 0;
 	for (int i = 0; i < N; ++i)
 	{
 		for (int j = 0; j < M; ++j)
 		{
-			cin >> originboard[i][j];
-			// cctv인 경우
-			if (originboard[i][j] != 6 && originboard[i][j] != 0)
-				cctv.push_back(make_pair(i, j));
-			// 사각지대
-			if (originboard[i][j] == 0)
-				Min++;
+			if (board[i][j] == 1)
+				result++;
 		}
 	}
+	cout << result;
 
-	// cctv 개수. cctv 각각의 방향을 0~4까지의 숫자로 나타내어 모든 경우의 수를 체크할 것이다. 
-	int cctvcount = cctv.size();
-	int maxdir = 1;
-	for (int i = 0; i < cctvcount; ++i)
-	{
-		maxdir *= 4;
-	}
-
-	// 만들 수 있는 경우의 수가 maxdir임. pow는 안쓰는게 안전하고, 비트쉬프트 연산자를 사용해서 1 << 2 * cctvcount를 maxdir로 사용해도 됨.
-	for (int i = 0; i <= maxdir; ++i)
-	{
-		// 카피보드 초기화
-		copyboard = originboard;
-		int temp = i;
-		// 각 cctv의 방향을 정하기 위해 1자리씩 잘라서 사용
-		for (int j = 0; j < cctvcount; ++j)
-		{
-			// 4진수로 temp를 사용하기 위해 4로 나눈 나머지를 사용한다. 
-			int dir = temp % 4;
-			if (originboard[cctv[j].first][cctv[j].second] == 1)
-			{
-				look(cctv[j].first, cctv[j].second, dir);
-			}
-			else if (originboard[cctv[j].first][cctv[j].second] == 2)
-			{
-				look(cctv[j].first, cctv[j].second, dir);
-				look(cctv[j].first, cctv[j].second, dir +2);
-			}
-			else if (originboard[cctv[j].first][cctv[j].second] == 3)
-			{
-				look(cctv[j].first, cctv[j].second, dir);
-				look(cctv[j].first, cctv[j].second, dir + 1);
-			}
-			else if (originboard[cctv[j].first][cctv[j].second] == 4)
-			{
-				look(cctv[j].first, cctv[j].second, dir);
-				look(cctv[j].first, cctv[j].second, dir + 1);
-				look(cctv[j].first, cctv[j].second, dir + 2);
-			}
-			else if (originboard[cctv[j].first][cctv[j].second] == 5)
-			{
-				look(cctv[j].first, cctv[j].second, dir);
-				look(cctv[j].first, cctv[j].second, dir + 1);
-				look(cctv[j].first, cctv[j].second, dir + 2);
-				look(cctv[j].first, cctv[j].second, dir + 3);
-			}
-
-			temp /= 4;
-		}
-
-		int tempmin = 0;
-		for (int i = 0; i < N; ++i)
-		{
-			for (int j = 0; j < M; ++j)
-			{
-				if (copyboard[i][j] == 0)
-					tempmin++;
-			}
-		}
-		// Min과 tempmin 중 작은 값으로 Min 설정.
-		Min = min(Min, tempmin);
-	}
-
-	cout << Min;
-
-	return 0;
+	return 0; 
 }
