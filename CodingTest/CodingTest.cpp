2a0d4e4f91c559fb7e8e701856417aa0f4189d46
l980l <kimkwanhyeon98@gmail.com>
2024-03-07 21:36:52 +0900

    BOJ 2048 (Easy) 하다가 중단

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,62 +1,17 @@
-﻿#include <iostream>	
+﻿#include <iostream>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
-int N, M, K;
-vector<vector<bool>> board;
-queue<vector<vector<bool>>> stickers;
+int N;
+int Max;
+vector<vector<int>> board;
+vector<vector<int>> copyboard;
 
-// 0번 스티커 Turn 시키기.
-void TurnSticker(vector<vector<bool>>& sticker)
+void Move(int dir)
 {
-	int SizeX = sticker.size();	// 열 길이
-	int SizeY = sticker.front().size();	// 어차피 직사각형이라 모든 행의 길이가 같음. 
-	// 새로운 스티커
-	vector<vector<bool>> NewSticker;
-	for (int i = 0; i < SizeY; ++i)
-	{
-		vector<bool> temp1;
-		for (int j = SizeX -1; j >= 0; --j)
-		{
-			temp1.push_back(sticker[j][i]);
-		}
-		NewSticker.push_back(temp1);
-	}
-	// 새로 만든 스티커로 바꾸기
-	sticker = NewSticker;
-}
 
-// 스티커의 LeftTop 좌표 값. 
-bool Stick(int x, int y, const vector<vector<bool>>& sticker)
-{
-	int SizeX = sticker.size();	// 열 길이
-	int SizeY = sticker.front().size();	// 어차피 직사각형이라 모든 행의 길이가 같음. 
-	for (int i = 0; i < SizeX; ++i)
-	{
-		for (int j = 0; j < SizeY; ++j)
-		{
-			int nx = x + i;
-			int ny = y + j;
-			// 노트북 넘어감. 
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-				return false;
-			// 붙일 수 없음.
-			if (board[nx][ny] == 1 && sticker[i][j] == 1)
-				return false;
-		}
-	}
-	// 붙일 수 있는 경우
-	for (int i = 0; i < SizeX; ++i)
-	{
-		for (int j = 0; j < SizeY; ++j)
-		{
-			if (board[x + i][y + j] == 0 && sticker[i][j] == 1)
-				board[x + i][y + j] = 1;
-		}
-	}
-	return true;
 }
 
 int main()
@@ -64,71 +19,54 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> N >> M >> K;
-	board = vector<vector<bool>>(N, vector<bool>(M));
-
-	for (int i = 0; i < K; ++i)
-	{
-		int x, y;
-		cin >> x >> y;
-		vector<vector<bool>> temp1;
-		for (int c = 0; c < x; ++c)
-		{
-			vector<bool> temp2;
-			for (int r = 0; r < y; ++r)
-			{
-				bool input;
-				cin >> input;
-				temp2.push_back(input);
-			}
-			temp1.push_back(temp2);
-		}
-		stickers.push(temp1);
-	}
-
-	while (!stickers.empty())
-	{
-		vector<vector<bool>> sticker = stickers.front();
-		stickers.pop();
-		bool isSticked = false;
-		int TurnCount = 0;
-
-		// 붙였거나, 4번 회전시켰거나.
-		while (!isSticked && TurnCount < 4)
-		{
-			for (int i = 0; i < N; ++i)
-			{
-				for (int j = 0; j < M; ++j)
-				{
-					// 스티커의 크기를 고려해서 LT의 위치로 가능한 위치를 확인해본다.
-					if (Stick(i, j, sticker))
-					{
-						// 스티커 붙이기 성공.
-						isSticked = true;
-						break;
-					}
-				}
-				if (isSticked)
-					break;
-			}
-			if (isSticked)
-				break;
-			// 회전시키기.
-			TurnSticker(sticker);
-			TurnCount++;
-		}
-	}
-
-	int result = 0;
+	cin >> N;
+	board = vector<vector<int>>(N, vector<int>(N));
 	for (int i = 0; i < N; ++i)
 	{
-		for (int j = 0; j < M; ++j)
+		for (int j = 0; j < N; ++j)
 		{
-			if (board[i][j] == 1)
-				result++;
+			cin >> board[i][j];
 		}
 	}
-	cout << result;
 
-	return 0; 
+	// 5번의 swipe 중 나올 수 있는 방향의 가지 수는 4의 5승이다.
+	for (int i = 0; i < (1 << 2 * 5); ++i)
+	{
+		copyboard = board;
+		int temp = i;
+		for (int j = 0; j < 5; ++j)
+		{
+			int dir = temp % 4;
+			temp /= 4;
+			if (dir == 0)
+			{
+
+			}
+
+			else if (dir == 1)
+			{
+
+			}
+
+			else if (dir == 2)
+			{
+
+			}
+
+			else if (dir == 3)
+			{
+
+			}
+		}
+		for (int h = 0; h < N; ++h)
+		{
+			for (int w = 0; w < N; ++w)
+			{
+				Max = max(Max, copyboard[h][w]);
+			}
+		}
+	}
+	cout << Max;
+
+	return 0;
 }
