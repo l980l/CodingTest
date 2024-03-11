33c58f57b25d06437ff1e50a1eff238c54d2321d
l980l <k910208@gmail.com>
2024-03-11 19:17:55 +0900

    BOJ 14499 주사위 굴리기

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,94 +1,108 @@
-﻿#include <iostream>
+﻿#include <iostream>	
 #include <vector>
 
 using namespace std;
 
-vector<vector<int>> Gear(4);	// 각 기어 배열
-vector<int> TopIdx(4);			// 각 기어의 12시 인덱스
+int N, M;
+int X, Y;
+//   2
+// 4 1 3
+//   5
+//   6
+// 문제에서는 주사위의 인덱스를 1 ~ 6으로 표현하지만 난 0 ~ 5로 함.
+int dice[6] = {};
+vector<vector<int>> board;
+// 동 서 북 남. x, y가 r, c라서 주의해야 한다.
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { 1, -1, 0, 0 };
 
-// 지금 Rotate 함수를 호출하게 만든 Gear를 prevG로 받음. 같은 경우에는 prevG가 없는 것.
-void Rotate(int GIdx, int dir, int prevG)
+void Move(int dir)
 {
-	bool LeftRot = false;
-	bool RightRot = false;
+	int nx = X + dx[dir];
+	int ny = Y + dy[dir];
 
-	// 왼쪽 기어 검사
-	if (GIdx <= 3 && GIdx > 0 && GIdx <= prevG)
+	// 화면 밖이면 안해
+	if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+		return;
+	// 이동시켜
+	X = nx;
+	Y = ny;
+
+	// 우선 주사위를 굴려.
+	// 동. 3 0 2 5을 5 3 0 2
+	if (dir == 0)
 	{
-		int Top1 = TopIdx[GIdx - 1] + 2;
-		int Top2 = TopIdx[GIdx] + 6;
-		if (Top1 > 7)
-			Top1 -= 8;
-		if (Top2 > 7)
-			Top2 -= 8;
-
-		if (Gear[GIdx - 1][Top1] != Gear[GIdx][Top2])
-			LeftRot = true;
+		int temp[4] = { dice[5], dice[3],dice[0],dice[2] };
+		dice[3] = temp[0];
+		dice[0] = temp[1];
+		dice[2] = temp[2];
+		dice[5] = temp[3];
+	}
+	// 서. 3 0 2 5을 0 2 5 3
+	else if (dir == 1)
+	{
+		int temp[4] = { dice[0], dice[2],dice[5],dice[3] };
+		dice[3] = temp[0];
+		dice[0] = temp[1];
+		dice[2] = temp[2];
+		dice[5] = temp[3];
+	}
+	// 북. 1, 0, 4, 5을 0, 4, 5, 1
+	else if (dir == 2)
+	{
+		int temp[4] = { dice[0], dice[4],dice[5],dice[1] };
+		dice[1] = temp[0];
+		dice[0] = temp[1];
+		dice[4] = temp[2];
+		dice[5] = temp[3];
+	}
+	// 남. 1, 0, 4, 5을 5, 1, 0, 4
+	else if (dir == 3)
+	{
+		int temp[4] = { dice[5], dice[1],dice[0],dice[4] };
+		dice[1] = temp[0];
+		dice[0] = temp[1];
+		dice[4] = temp[2];
+		dice[5] = temp[3];
 	}
 
-	// 오른쪽 기어 검사
-	if (GIdx >= 0 && GIdx < 3 && GIdx >= prevG)
+	// 지도랑 상호작용.
+	if (board[X][Y] == 0)
 	{
-		int Top1 = TopIdx[GIdx] + 2;
-		int Top2 = TopIdx[GIdx + 1] + 6;
-		if (Top1 > 7)
-			Top1 -= 8;
-		if (Top2 > 7)
-			Top2 -= 8;
-
-		if (Gear[GIdx][Top1] != Gear[GIdx + 1][Top2])
-			RightRot = true;
+		board[X][Y] = dice[5];
 	}
-
-	TopIdx[GIdx] += dir;
-	// Top 인덱스가 음수면 8을 더해서 양수로 만들어 주기.
-	if (TopIdx[GIdx] < 0)
-		TopIdx[GIdx] += 8;
-	if (TopIdx[GIdx] > 7)
-		TopIdx[GIdx] -= 8;
-
-	// 이렇게 따로 LeftRot, RightRot을 만들어서 호출하는 이유는 TopIdx[GIdx]을 변경하기 전에 양 옆 기어의 회전 여부를 구해둬야 하기 때문이다.
-	if (LeftRot)
-		Rotate(GIdx - 1, dir * -1, GIdx);
-	if (RightRot)
-		Rotate(GIdx + 1, dir * -1, GIdx);
+	else
+	{
+		dice[5] = board[X][Y];
+		board[X][Y] = 0;
+	}
+	// 윗 면에 쓰인 수 출력
+	cout << dice[0] << '\n';
 }
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	
+	int K;
+	cin >> N >> M >> X >> Y >> K;
 
-	for (int i = 0; i < 4; ++i)
+	board = vector<vector<int>>(N, vector<int>(M));
+	for (int i = 0; i < N; ++i)
 	{
-		string temp;
-		cin >> temp;
-		for (char c : temp)
+		for (int j = 0; j < M; ++j)
 		{
-			Gear[i].push_back(c - '0');
+			cin >> board[i][j];
 		}
 	}
 
-	int K;
-	cin >> K;
 	for (int i = 0; i < K; ++i)
 	{
-		// 톱니 번호, 회전 방향(1이 시계방향, -1이 반시계방향)
-		int x, y;
-		cin >> x >> y;
-		// 나는 기어번호를 0~3으로 쓰고 문제는 1부터 4로 써서 -1을 해줌.
-		// 1이 시계방향이고 -1이 반시계방향이라 반대로 넣어주려고 -1을 곱함.
-		Rotate(x - 1, -y, x - 1);
+		int dir;
+		cin >> dir;
+		Move(dir - 1);
 	}
-
-	int Result = 0;
-	for (int i = 0; i < 4; ++i)
-	{
-		// 비트 쉬프트 연산자 이용해서 1 2 4 8 더하도록 하기
-		if (Gear[i][TopIdx[i]])
-			Result += (1 << i);
-	}
-	cout << Result;
 
 	return 0;
 }
