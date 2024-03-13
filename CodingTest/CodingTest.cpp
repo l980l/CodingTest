4bee1da5f35294fc5a9569aebec324868167c5a7
l980l <kimkwanhyeon98@gmail.com>
2024-03-13 21:01:17 +0900

    BOJ 3190 뱀

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,77 +1,89 @@
-﻿#include <iostream>	
-#include <vector>	
+﻿#include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-int N, M;
-int r, c, d;	// 문제에서 d는 0 북 1 동 2 남 3 서 이지만 북,서,남,동으로 사용할거임.
-vector<vector<int>> board;
-vector<vector<bool>> cleaned;
-int result = 0;
-// 회전을 반시계방향으로 90도씩 시키기 때문에 북,서,남,동을 가리키도록 만듦.
+int N, K, L;
+vector<vector<int>> board;	// 사과가 있으면 1, 없으면 0, 뱀이 있으면 2
+int X = 0, Y = 0;
+int Sec = 0;
+queue<pair<int, char>> Moving;
+int Dir = 1; // 0 북 1 동 2 남 3 서
 int dx[4] = { -1,0,1,0 };
-int dy[4] = { 0,-1,0,1 };
+int dy[4] = { 0,1,0,-1 };
+queue<pair<int, int>> Snake;	// 뱀 위치.
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> N >> M;
-	cin >> r >> c >> d;
-	// 편의를 위해 d를 북 서 남 동으로 사용할 것이기 때문에 
-	if (d == 1)
-		d = 3;
-	else if (d == 3)
-		d = 1;
-	board = vector<vector<int>>(N, vector<int>(M));
-	cleaned = vector<vector<bool>>(N, vector<bool>(M));
-	for (int i = 0; i < N; ++i)
+	cin >> N >> K;
+	board = vector<vector<int>>(N, vector<int>(N));
+	for (int i = 0; i < K; ++i)
 	{
-		for (int j = 0; j < M; ++j)
-		{
-			cin >> board[i][j];
-		}
+		int x, y;
+		cin >> x >> y;
+		board[x-1][y-1] = 1;
+	}
+	cin >> L;
+	for (int i = 0; i < L; ++i)
+	{
+		int x;
+		char c;
+		cin >> x >> c;
+		Moving.push(make_pair(x, c));
 	}
 
-	result++;
-	cleaned[r][c] = true;
+	board[X][Y] = 2;
+	Snake.push(make_pair(X, Y));
+	auto pair = Moving.front();
 	while (true)
 	{
-		int FD;
-		bool cleaning = false;
-		// 무조건 회전을 먼저 하는거였네;; 이 조건 못찾아서 1시간 이상 날림;;
-		for (int i = 1; i <= 4; ++i)
-		{
-			FD = (d + i) % 4;
-			int nx = r + dx[FD];
-			int ny = c + dy[FD];
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M || board[nx][ny] == 1 || cleaned[nx][ny] == true)
-				continue;
-			r = nx;
-			c = ny;
-			d = FD;
-			cleaned[nx][ny] = true;
-			result++;
-			cleaning = true;
+		// 이동
+		X += dx[Dir];
+		Y += dy[Dir];
+		// Sec 늘리기                                                                       
+		Sec++;
+		// 종료
+		if (X < 0 || X >= N || Y < 0 || Y >= N || board[X][Y] == 2)
 			break;
+		
+		// 사과가 아니야. 그럼 꼬리를 잘라.
+		if (board[X][Y] == 0)
+		{                                                                                               
+			auto Tail = Snake.front();
+			Snake.pop();
+			board[Tail.first][Tail.second] = 0;
 		}
-		// 청소 성공 했으면 다음 구역으로.
-		if (cleaning)
-			continue;
-		// 역방향
-		FD = (d + 2) % 4;
-		int nx = r + dx[FD];
-		int ny = c + dy[FD];
-		// 청소 종료
-		if (nx < 0 || nx >= N || ny < 0 || ny >= M || board[nx][ny] == 1)
-			break;
-		// 청소는 못하고 뒤로 가는 경우. d는 그대로 유지해야 한다. 
-		r = nx;
-		c = ny;
+		// 뱀으로 추가.
+		board[X][Y] = 2;
+		Snake.push(make_pair(X, Y));
+
+		// 방향 바꾸기.
+		if (Sec == pair.first)
+		{
+			// 오른쪽으로 90도 회전
+			if (pair.second == 'D')
+			{
+				Dir = (Dir + 1) % 4;
+			}
+			// 왼쪽으로 90도 회전
+			else
+			{
+				// 4를 더한 다음 -1을 해서 음수가 나오지 않도록 처리
+				Dir = (Dir + 4 - 1) % 4;
+			}
+			Moving.pop();
+			if (!Moving.empty())
+			{
+				pair = Moving.front();
+			}
+		}
 	}
 
-	cout << result;
+	cout << Sec;
 
 	return 0;
 }
