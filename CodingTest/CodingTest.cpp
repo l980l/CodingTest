257c71eaadca27e60499adc89dbb5069bda6cb94
l980l <k910208@gmail.com>
2024-03-15 22:13:53 +0900

    BOJ 13460 구슬 탈출 2

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -8,90 +8,278 @@ vector<string> board; // 보드 판은 그대로 있고, R과 B의 위치만 이
 int ORX, ORY, OBX, OBY;
 int RX, RY, BX, BY;
 int Min = 2147483647;
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
 
-// R 굴리기
-void MoveR(int dir)
+// R 굴리기. 안움직이면 -1 움직이면 0 구멍으로 나갔으면 1
+int MoveR(int dir)
 {
-
+	int move = -1;
+	if (dir == 0)
+	{
+		while (true)
+		{
+			int nx = RX - 1;
+			if (board[nx][RY] == 'O')
+			{
+				RX = nx;
+				return 1;
+			}
+			else if (board[nx][RY] == '#' || BX == nx && RY == BY)
+				break;
+			else
+			{
+				RX = nx;
+				move = 0;
+			}
+		}
+	}
+	else if (dir == 1)
+	{
+		while (true)
+		{
+			int ny = RY + 1;
+			if (board[RX][ny] == 'O')
+			{
+				RY = ny;
+				return 1;
+			}
+			else if (board[RX][ny] == '#' || BX == RX && ny == BY)
+				break;
+			else
+			{
+				RY = ny;
+				move = 0;
+			}
+		}
+	}
+	else if (dir == 2)
+	{
+		while (true)
+		{
+			int nx = RX + 1;
+			if (board[nx][RY] == 'O')
+			{
+				RX = nx;
+				return 1;
+			}
+			else if (board[nx][RY] == '#' || BX == nx && RY == BY)
+				break;
+			else
+			{
+				RX = nx;
+				move = 0;
+			}
+		}
+	}
+	else if (dir == 3)
+	{
+		while (true)
+		{
+			int ny = RY - 1;
+			if (board[RX][ny] == 'O')
+			{
+				RY = ny;
+				return 1;
+			}
+			else if (board[RX][ny] == '#' || BX == RX && ny == BY)
+				break;
+			else
+			{
+				RY = ny;
+				move = 0;
+			}
+		}
+	}
+	return move;
 }
 // B 굴리기
-void MoveB(int dir)
+int MoveB(int dir)
 {
-
+	int move = -1;
+	if (dir == 0)
+	{
+		while (true)
+		{
+			int nx = BX - 1;
+			if (board[nx][BY] == 'O')
+			{
+				BX = nx;
+				return 1;
+			}
+			else if (board[nx][BY] == '#' || nx == RX && BY == RY)
+				break;
+			else
+			{
+				BX = nx;
+				move = 0;
+			}
+		}
+	}
+	else if (dir == 1)
+	{
+		while (true)
+		{
+			int ny = BY + 1;
+			if (board[BX][ny] == 'O')
+			{
+				BY = ny;
+				return 1;
+			}
+			else if (board[BX][ny] == '#' || BX == RX && ny == RY)
+				break;
+			else
+			{
+				BY = ny;
+				move = 0;
+			}
+		}
+	}
+	else if (dir == 2)
+	{
+		while (true)
+		{
+			int nx = BX + 1;
+			if (board[nx][BY] == 'O')
+			{
+				BX = nx;
+				return 1;
+			}
+			else if (board[nx][BY] == '#' || nx == RX && BY == RY)
+				break;
+			else
+			{
+				BX = nx;
+				move = 0;
+			}
+		}
+	}
+	else if (dir == 3)
+	{
+		while (true)
+		{
+			int ny = BY - 1;
+			if (board[BX][ny] == 'O')
+			{
+				BY = ny;
+				return 1;
+			}
+			else if (board[BX][ny] == '#' || BX == RX && ny == RY)
+				break;
+			else
+			{
+				BY = ny;
+				move = 0;
+			}
+		}
+	}
+	return move;
 }
 
 // 구술이 탈출하면 true 반환. 0 1 2 3북 동 남 서
 // 이동 경로 상으로 일직선상에 위치하면 앞에 있는 애가 먼저 움직이는 방식으로 처리해야 할듯? 따라서 북쪽이나 남쪽으로 이동하는 거면, R과 B가 같은 행인지 확인하고, 북쪽이면서 같은 행이면 x가 더 낮은 구슬부터, 남쪽이면 x가 더 높은 구슬부터. 동 서의 경우에는 같은 열인지 확인, 서쪽의 경우 y가 더 낮은 구슬, 동쪽의 경우 더 높은 구슬부터.
-bool Tilt(int dir)
+// 안움직이면 -1, 움직이면 0, R만 구멍으로 나갔으면 1, B가 나와도 -1
+int Tilt(int dir)
 {
+	int R, B;
 	// 북
 	if (dir == 0)
 	{
-		if (ORY == OBY)
+		if (RY == BY)
 		{
-			if (ORX > OBX)
+			if (RX > BX)
 			{
-				MoveB(dir);
-				MoveR(dir);
+				B = MoveB(dir);
+				R = MoveR(dir);
+			}
+			else
+			{
+				R = MoveR(dir);
+				B = MoveB(dir);
 			}
 		}
 		else
 		{
-			MoveR(dir);
-			MoveB(dir);
+			R = MoveR(dir);
+			B = MoveB(dir);
 		}
 	}
 	// 동
 	else if (dir == 1)
 	{
-		if (ORX == OBX)
+		if (RX == BX)
 		{
-			if (ORY < OBY)
+			if (RY < BY)
 			{
-				MoveB(dir);
-				MoveR(dir);
+				B = MoveB(dir);
+				R = MoveR(dir);
+			}
+			else
+			{
+				R = MoveR(dir);
+				B = MoveB(dir);
 			}
 		}
 		else
 		{
-			MoveR(dir);
-			MoveB(dir);
+			R = MoveR(dir);
+			B = MoveB(dir);
 		}
 	}
 	// 남
 	else if (dir == 2)
 	{
-		if (ORY == OBY)
+		if (RY == BY)
 		{
-			if (ORX < OBX)
+			if (RX < BX)
 			{
-				MoveB(dir);
-				MoveR(dir);
+				B = MoveB(dir);
+				R = MoveR(dir);
+			}
+			else
+			{
+				R = MoveR(dir);
+				B = MoveB(dir);
 			}
 		}
 		else
 		{
-			MoveR(dir);
-			MoveB(dir);
+			R = MoveR(dir);
+			B = MoveB(dir);
 		}
 	}
 	// 서
 	else if (dir == 3)
 	{
-		if (ORX == OBX)
+		if (RX == BX)
 		{
-			if (ORY > OBY)
+			if (RY > BY)
 			{
-				MoveB(dir);
-				MoveR(dir);
+				B = MoveB(dir);
+				R = MoveR(dir);
+			}
+			else
+			{
+				R = MoveR(dir);
+				B = MoveB(dir);
 			}
 		}
 		else
 		{
-			MoveR(dir);
-			MoveB(dir);
+			R = MoveR(dir);
+			B = MoveB(dir);
 		}
 	}
+
+	// 음 근데 안 움직이는건 4방향으로 tilt 했을 때, 전부 -1이 나와야 되는건데, 굳이 처리할 필요 있나? 그냥 파랑이 나간 경우만 -1로 반환하자.
+	/*if (R == -1 && B == -1)
+		return -1;*/
+	if (B == 1)
+		return -1;
+	if (R == 1)
+		return 1;
+	else
+		return 0;
 }
 
 int main()
@@ -106,6 +294,7 @@ int main()
 		for (int j = 0; j < M; ++j)
 		{
 			char c;
+			cin >> c;
 			// R이나 B 구슬이면 위치만 따로 저장하고, board에는 빈칸으로 만듦.
 			if (c == 'R')
 			{
@@ -119,7 +308,6 @@ int main()
 				OBY = j;
 				c = '.';
 			}
-			cin >> c;
 			board[i].push_back(c);
 		}
 	}
@@ -142,11 +330,15 @@ int main()
 			temp /= 4;
 			CurTiltCount++;
 			// 기울여서 R이 나왔다면 true. 그 경우 최소값 확인후 갱신.
-			if (Tilt(dir))
+			int Out = Tilt(dir);
+			if (Out == 1)
 			{
 				Min = min(Min, CurTiltCount);
 				break;
 			}
+			// B가 나온 경우.
+			else if (Out == -1)
+				break;
 		}
 	}
 
