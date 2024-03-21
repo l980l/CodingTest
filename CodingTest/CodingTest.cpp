964e7f91742892845c2ccee146475afef1b83373
l980l <k910208@gmail.com>
2024-03-21 23:19:20 +0900

    BOJ 2819 빈도 정렬

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <string>
+#include <tuple>
 
 using namespace std;
 
@@ -10,23 +10,48 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n;
-	cin >> n;
-	vector<long long> Nums;
-	for (int i = 0; i < n; ++i)
+	int N;
+	long long C;
+	cin >> N >> C;
+	vector<pair<long long, int>> Num(N);
+	for (int i = 0; i < N; ++i)
 	{
-		string temp;
+		long long temp;
 		cin >> temp;
-		int size = (int)temp.size();
-		reverse(temp.begin(), temp.end());
-		// stoll는 string을 long long으로 바꿔주는 함수인데, string 헤더를 include 해야 사용할 수 있음. 
-		Nums.push_back(stoll(temp));
+		Num[i] = make_pair(temp, i); 
 	}
-	sort(Nums.begin(), Nums.end());
-	for (long long i : Nums)
-	{
-		cout << i <<'\n';
-	}
+	sort(Num.begin(), Num.end());
 
+	vector<tuple<int, int, long long>> SortedNum;
+	long long prev = Num[0].first;
+	SortedNum.push_back(make_tuple(1, N - Num[0].second,prev)); // 2번 인자는 나중에 greater로 오름차순으로 정렬되야 하는데, 앞에서 나온걸 먼저 두고 싶기 때문에 앞에 올수록 큰수로 만듦.
+	for (int i = 1; i < N; ++i)
+	{
+		if (Num[i].first == prev)
+		{
+			int x, y;
+			long long ll;
+			tie(x, y, ll) = SortedNum.back();
+			SortedNum.back() = make_tuple(x + 1, y, ll);
+		}
+		else
+		{
+			prev = Num[i].first;
+			SortedNum.push_back(make_tuple(1, N - Num[i].second, prev));
+		}
+	}
+	// 이번엔 오름차 순으로.
+	sort(SortedNum.begin(), SortedNum.end(), greater<tuple<int, int, long long>>());
+	auto iter = SortedNum.begin();
+	for (; iter != SortedNum.end(); ++iter)
+	{
+		int x, y;
+		long long ll;
+		tie(x, y, ll) = *iter;
+		for (int i = 0; i < x; ++i)
+		{
+			cout << ll << ' ';
+		}
+	}
 	return 0;
 }
