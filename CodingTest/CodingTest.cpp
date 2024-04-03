eab5bd3a8631e18a277f086a4cfc890e61500cce
l980l <k910208@gmail.com>
2024-04-03 17:51:28 +0900

    BOJ 10844 쉬운 계단 수

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -11,34 +11,39 @@ int main()
 
 	int N;
 	cin >> N;
-	vector<int> T(N + 2);
-	vector<int> P(N + 2);
-	for (int i = 1; i < N + 1; ++i)
+
+	// D[i][k]. 길이가 i인 계단 수 중에서 마지막 숫자가 k로 끝나는 수의 개수를 저장.
+	vector<vector<long long>> D(101, vector<long long>(10));
+	// 길이가 1인 계단 수는 0을 제외하고 각각 1개씩 있다.
+	for (int i = 1; i < 10; ++i)
 	{
-		cin >> T[i];
-		cin >> P[i];
+		D[1][i] = 1;
 	}
 
-	// i 번째 일에 상담을 시작했을 때 얻을 수 있는 최대 수익.
-	vector<long long> D(N + 2);
-	// 뒤쪽부터 검사하면, 이전 날짜의 상담에 영향을 받지 않을 수 있다. 
-	for (int i = N; i >= 1; --i)
+	// 길이가 2부터 N인 수에 대해서는 마지막 숫자가 0에서 9까지 가능하다.
+	for (int i = 2; i <= N; ++i)
 	{
-		// 기간 안에 할 수 있는 상담인지.
-		if (T[i] + i <= N + 1)
+		for (int k = 0; k < 10; ++k)
 		{
-			// 현재 상담이 끝나는 날짜의 최대 수익 + 현재 상담 수익과 다음날 최대 상담 수익을 비교하여 큰 값으로 세팅.
-			D[i] = max(D[i + T[i]] + P[i], D[i + 1]);
-		}
-		else
-		{
-			D[i] = D[i + 1];
+			// k가 0이 아닌 경우, k-1로 끝나는 계단 수 뒤에 k를 붙여 새로운 계단 수를 만든다.
+			if (k != 0)
+				D[i][k] += D[i - 1][k - 1];
+			// k가 9가 아닌 경우, k+1로 끝나는 계단 수 뒤에 k를 붙여 새로운 계단 수를 만든다.
+			if (k != 9)
+				D[i][k] += D[i - 1][k + 1];
+
+			D[i][k] %= 1000000000;
 		}
 	}
 
-	// 근데 이걸 왜 max_element를 쓰는걸까... 그냥 D[1] 아니야? 
-	//cout << *max_element(D.begin() + 1, D.end());
-	cout << D[1];
+	long long result = 0; 
+	// 길이가 N인 모든 계단 수를 더한다.
+	for (int i = 0; i <= 9; ++i)
+	{
+		result += D[N][i];
+	}
+	result %= 1000000000;
+	cout << result;
 
 	return 0;
 }
