65f8f801cf404c605efb33574cf56a790b62cc9a
l980l <k910208@gmail.com>
2024-04-09 04:11:04 +0900

    BOJ 1915 가장 큰 정사각형

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -9,37 +9,38 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int T;
-	cin >> T;
-	while (T--)
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<int>> board(n + 1, vector<int>(m + 1));
+	// D[i][j]: i,j 좌표를 오른쪽 아래로 하는 정사각형의 한 변의 크기.
+	vector<vector<int>> D(n + 1, vector<int>(m + 1));
+
+	for (int i = 1; i <= n; ++i)
 	{
-		int N;
-		cin >> N;
-		vector<int> C(N);
-		// i를 만들 수 있는 방법의 수. M의 단위가 10000까지니까. M 자체로 크기를 정해도 되지만, 화폐단위를 입력 받을 때 바로 처리해야 하기 때문에 미리 크기를 정해둠.
-		vector<long long> D(10001);
-		for (int i = 0; i < N; ++i)
+		string temp;
+		cin >> temp;
+		for (int j = 1; j <= m; ++j)
 		{
-			cin >> C[i];
+			board[i][j] = temp[j] - '0';
 		}
-		int M;
-		cin >> M;
-		// 이게 중요하다. 금액이 0인 경우는 문제로 나오지도 않는데, 기반 조건을 만들기 위해 설정해야 한다. 이렇게 해야, D[화폐 단위]에 1이 더해질 것이다. 
-		// 이 방법이 이해가 안된다면, 아마 모든 화폐 단위에 대해 초기 D 값을 1로 두면 될 것 같다.
-		D[0] = 1;
-		// 모든 코인들에 반복.
-		for (int i = 0; i < N; ++i)
+	}
+	
+	int result = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= m; ++j)
 		{
-			// 현재 코인 금액부터, 목표 금액까지.
-			// D[현재 금액 j] += D[현재 금액 - 화폐 단위]
-			for (int j = C[i]; j <= M; ++j)
+			if (board[i][j])
 			{
-				D[j] += D[j - C[i]];
+				// 좌상단, 상단, 좌측 3군데 중 최소 D 값에 + 1을 해야 정사각형을 만들 수 있다.
+				D[i][j] = min({ D[i][j - 1], D[i - 1][j], D[i - 1][j - 1] }) + 1;
+				result = max(result, D[i][j]);
 			}
 		}
-		cout << D[M] << '\n';
 	}
 
+	cout << result * result;
 
 	return 0;
 }
