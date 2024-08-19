ce573cb6aff516745b5c31e46eaf68f6fb8e4053
l980l <kimkwanhyeon98@gmail.com>
2024-08-19 22:00:16 +0900

    BOJ 2003 수들의 합 2

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -3,82 +3,55 @@
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	int N;
-	cin >> N;
+    int N;
+    long long M;
+    cin >> N >> M;
 
-	// N이 1이면 0 바로 반환.
-	if (N == 1)
-	{
-		cout << 0;
-		return 0;
-	}
+    vector<int> Num;
+    for (int i = 0; i < N; ++i)
+    {
+        int temp;
+        cin >> temp;
+        Num.push_back(temp);
+    }
 
-	// 에라스토테네스의 체
-	vector<bool> vecIsPrime(N + 1, true);
-	vector<int> vecPrime;
+    long long sum = Num[0];
+    int st = 0;
+    int en = 1;
+    int result = 0;
 
-	vecIsPrime[2] = true;
+    while (true)
+    {
+        if (sum == M)
+            result++;
+        if (sum <= M)
+        {
+            if (en < int(Num.size()))
+                sum += Num[en++];
+        }
+        if (sum > M)
+            sum -= Num[st++];
+        if (en >= int(Num.size()))
+        {
+            if (sum == M)
+                result++;
+            break;
+        }
+    }
 
-	for (int i = 2; i * i <= N; ++i)
-	{
-		if (vecIsPrime[i])
-		{
-			for (int j = i * i; j <= N; j += i)
-			{
-				vecIsPrime[j] = false;
-			}
-		}
-	}
+    if (N == 1)
+    {
+        if (Num[0] == M)
+            result = 1;
+        else
+            result = 0;
+    }
 
-	for (int i = 2; i <= N; ++i)
-	{
-		if (vecIsPrime[i])
-			vecPrime.push_back(i);
-	}
+    cout << result;
 
-	int st = 0;
-	int en = 0;
-
-	int count = 0;
-	int sum = 0;
-
-	sum += vecPrime[en];
-
-	while (st <= en && en < vecPrime.size())
-	{
-		if (sum == N)
-		{
-			count++;
-			st++;
-			en++;
-
-			if (en < vecPrime.size())
-				sum = sum - vecPrime[st - 1] + vecPrime[en];
-		}
-
-		else if (sum < N)
-		{
-			en ++;
-
-			if(en < vecPrime.size())
-				sum += vecPrime[en];
-		}
-
-		else if (sum > N)
-		{
-			st++;
-
-			if (st <= en)
-				sum -= vecPrime[st - 1];
-		}
-	}
-
-	cout << count;
-
-	return 0;		
+    return 0;
 }
