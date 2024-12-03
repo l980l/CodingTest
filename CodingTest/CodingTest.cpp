fd1d33a9bb06340f5712c252e1b765b068e97883
l980l <kimkwanhyeon98@gmail.com>
2024-12-03 11:45:59 +0900

    BOJ 20166 문자열 지옥에 빠진 호석

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,45 +1,59 @@
 ﻿#include <iostream>
+#include <vector>
 #include <unordered_map>
+#include <queue>
 
 using namespace std;
+
+int N, M, K;
+vector<string> board;
+unordered_map<string, int> strings;
+
+int dx[8] = { 1, 0, -1, 0, 1, -1, 1, -1 };
+int dy[8] = { 0, 1, 0, -1, 1, -1, -1, 1 };
+
+void func(int x, int y, string madeStr)
+{
+    ++strings[madeStr];
+    if (madeStr.size() >= 5)
+        return;
+    for (int i = 0; i < 8; ++i)
+    {
+        int nx = (x + dx[i] + N) % N;
+        int ny = (y + dy[i] + M) % M;
+        func(nx, ny, madeStr + board[nx][ny]);
+    }
+}
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int t, n;
-    cin >> t;
+    cin >> N >> M >> K;
+    board = vector<string>(N);
 
-    unordered_map<string, int> clothes;
-    string temp, temp2;
-
-    while (t--)
+    for (int i = 0; i < N; ++i)
     {
-        cin >> n;
-        clothes.clear();
+        cin >> board[i];
+    }
 
-        if (n == 0)
+    string madeStr;
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < M; ++j)
         {
-            cout << 0 << "\n";
-            continue;
+            madeStr = board[i][j];
+            func(i, j, madeStr);
         }
+    }
 
-        while (n--)
-        {
-            cin >> temp >> temp2;
-            ++clothes[temp2];
-        }
+    while (K--)
+    {
+        string temp;
+        cin >> temp;
 
-        int result = 0;
-        for (auto c : clothes)
-        {
-            if (result == 0)
-                result = c.second + 1;
-            else
-                result *= c.second + 1;
-        }
-        cout << result - 1 << "\n";
+        cout << strings[temp] << '\n';
     }
 
     return 0;
