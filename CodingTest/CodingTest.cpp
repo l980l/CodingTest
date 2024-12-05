8563db98a838c37a08d589116658a4d7a2c26433
l980l <kimkwanhyeon98@gmail.com>
2024-12-05 11:49:49 +0900

    BOJ 7662 이중 우선순위 큐

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,49 +1,57 @@
 ﻿#include <iostream>
-#include <unordered_set>
-#include <unordered_map>
-#include <algorithm>
+#include <set>
 
 using namespace std;
-
-long long N, P, Q;
-
-void func(long long n, unordered_set<long long>& set)
-{
-    if (n == 0)
-        return;
-
-    set.insert(n);
-    func(n / P, set);
-    func(n / Q, set);
-}
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    cin >> N >> P >> Q;
+    int T;
+    cin >> T;
 
-    unordered_map<long long, long long> DP;
-    DP[0] = 1;
-
-    vector<long long> idxList;
+    while (T--)
     {
-        unordered_set<long long> idxSet;
-        func(N, idxSet);
-        idxList = vector(idxSet.begin(), idxSet.end());
+        int k;
+        cin >> k;
+
+        set<int> Q;
+        while (k--)
+        {
+            char Op;
+            int Num;
+            cin >> Op >> Num;
+
+            if (Op == 'I')
+                Q.insert(Num);
+
+            else
+            {
+                if (Num == 1)
+                {
+                    if (Q.empty() == false)
+                        Q.erase(prev(Q.end()));
+                }
+
+                else
+                {
+                    if (Q.empty() == false)
+                        Q.erase(Q.begin());
+                }
+            }
+        }
+
+        if (Q.empty() == true)
+        {
+            cout << "EMPTY" << "\n";
+            continue;
+        }
+
+        else
+            cout << *prev(Q.end()) << " " << *Q.begin() << "\n";
     }
 
-    sort(idxList.begin(), idxList.end());
-
-    auto iter = idxList.begin();
-
-    for (long long i : idxList)
-    {
-        DP[i] = DP[i / P] + DP[i / Q];
-    }
-
-    cout << DP[N];
 
     return 0;
 }
