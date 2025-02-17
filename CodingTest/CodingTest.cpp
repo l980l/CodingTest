fd33885f25bf95122f4e5c0302caa2515e2888a1
l980l <kimkwanhyeon98@gmail.com>
2025-02-18 08:45:09 +0900

    BOJ 11286 절댓값 힙

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
-#include <set>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -8,125 +9,40 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int N, P, L, G, M;
-    pair<int, int> PN[100001];
-    set<int> PL[101];
-    set<int> PG[101][101];
-
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+    int N;
+    long long x;
     cin >> N;
 
-    while(N--)
+    while (N--)
     {
-        cin >> P >> L >> G;
-        PN[P] = { L,G };
-        PL[L].insert(P);
-        PG[G][L].insert(P);
-    }
+        cin >> x;
 
-    cin >> M;
-    string op;
-    int x;
-    while (M--)
-    {
-        cin >> op;
-        
-        if (op == "recommend")
+        if (x == 0)
         {
-            cin >> G >> x;
-            if (x == 1)
+            if (pq.empty())
+                cout << 0 << "\n";
+            else
             {
-                for (int i = 100; i >= 1; --i)
-                {
-                    if (PG[G][i].empty() == false)
-                    {
-                        cout << *(prev(PG[G][i].end())) << "\n";
-                        break;
-                    }
-                }
+                cout << pq.top().first * pq.top().second << "\n";
+                pq.pop();
+            }
+        }
+
+        else
+        {
+            pair<int, int> temp;
+            if (x > 0)
+            {
+                temp.first = x;
+                temp.second = 1;
             }
             else
             {
-                for (int i = 1; i < 101; ++i)
-                {
-                    if (PG[G][i].empty() == false)
-                    {
-                        cout << *PG[G][i].begin() << "\n";
-                        break;
-                    }
-                }
+                temp.first = -x;
+                temp.second = -1;
             }
-        }
-
-        else if (op == "recommend2")
-        {
-            cin >> x;
-            if (x == 1)
-            {
-                for (int i = 100; i >= 1; --i)
-                {
-                    if (PL[i].empty() == false)
-                    {
-                        cout << *(prev(PL[i].end())) << "\n";
-                        break;
-                    }
-                }
-            }
-
-            else
-            {
-                for (int i = 1; i <= 100; ++i)
-                {
-                    if (PL[i].empty() == false)
-                    {
-                        cout << *PL[i].begin() << "\n";
-                        break;
-                    }
-                }
-            }
-        }
-
-        else if (op == "recommend3")
-        {
-            cin >> x >> L;
-            int ans = -1;
-            if (x == 1)
-            {
-                for (int i = L; i < 101; ++i)
-                {
-                    if (PL[i].empty() == false)
-                    {
-                        ans = *PL[i].begin();
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                for (int i = L - 1; i >= 1; --i)
-                {
-                    if (PL[i].empty() == false)
-                    {
-                        ans = *(prev(PL[i].end()));
-                        break;
-                    }
-                }
-            }
-            cout << ans << "\n";
-        }
-
-        else if (op == "add")
-        {
-            cin >> P >> L >> G;
-            PN[P] = { L,G };
-            PL[L].insert(P);
-            PG[G][L].insert(P);
-        }
-
-        else if (op == "solved")
-        {
-            cin >> P;
-            PL[PN[P].first].erase(P);
-            PG[PN[P].second][PN[P].first].erase(P);
+            pq.push(temp);
         }
     }
 
