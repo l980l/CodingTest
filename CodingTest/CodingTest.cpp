417faa83b5717613d6691eaf2f867617807b05de
l980l <kimkwanhyeon98@gmail.com>
2025-02-23 13:15:10 +0900

    BOJ 1781 컵라면

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -9,44 +9,54 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // 작은 값들을 모아둔 최대힙 pq와 큰 값들을 모아둔 최소힙 pq로 해보자
-    priority_queue<int, vector<int>> sg;
-    priority_queue<int, vector<int>, greater<int>> lg;
-
-    int N, x;
+    int N, d, c;
     cin >> N;
 
-    cin >> x;
-    sg.push(x);
-    cout << sg.top() << "\n";
+    // 초기 정렬에는 pq가 아니라 vector에 넣고 sort 하는게 나을 거 같기도
+    vector<priority_queue<int, vector<int>>> vecPQ(N + 1);
 
-    for (int i = 1; i < N; ++i)
+    for (int i = 1; i <= N; ++i)
     {
-        cin >> x;
-
-        if (lg.empty() == false && lg.top() <= x)
-        {
-            lg.push(x);
-        }
-        else
-        {
-            sg.push(x);
-        }
-
-        if (sg.size() - lg.size() == -1)
-        {
-            sg.push(lg.top());
-            lg.pop();
-        }
-
-        else if (sg.size() - lg.size() == 2)
-        {
-            lg.push(sg.top());
-            sg.pop();
-        }
-
-        cout << sg.top() << "\n";
+        cin >> d >> c;
+        vecPQ[d].push(c);
     }
+
+    priority_queue<int, vector<int>, greater<int>> resultPQ;
+    long long result = 0;
+
+    for (int i = 1; i <= N; ++i)
+    {
+        if (vecPQ[i].empty() == true)
+            continue;
+
+        resultPQ.push(vecPQ[i].top());
+        result += vecPQ[i].top();
+        vecPQ[i].pop();
+
+        while (true)
+        {
+            if (resultPQ.size() < i && vecPQ[i].empty() == false)
+            {
+                resultPQ.push(vecPQ[i].top());
+                result += vecPQ[i].top();
+                vecPQ[i].pop();
+            }
+
+            else if (resultPQ.empty() == false && vecPQ[i].empty() == false && resultPQ.top() < vecPQ[i].top())
+            {
+                result -= resultPQ.top();
+                resultPQ.pop();
+                resultPQ.push(vecPQ[i].top());
+                result += vecPQ[i].top();
+                vecPQ[i].pop();
+            }
+
+            else
+                break;
+        }
+    }
+
+    cout << result;
 
     return 0;
 }
