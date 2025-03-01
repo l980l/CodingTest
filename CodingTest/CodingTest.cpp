adb5318d8682acb978629fd39acb852f4193da7f
l980l <kimkwanhyeon98@gmail.com>
2025-03-01 10:36:34 +0900

    BOJ 1707 이분 그래프

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -5,72 +5,73 @@
 
 using namespace std;
 
+int k, v, e;
+int gr[20001] = {};
+vector<int> adj[20001];
+
+bool solve()
+{
+    fill(gr, gr + v + 1, -1);
+
+    for (int i = 1; i <= v; ++i)
+    {
+        if (gr[i] != -1)
+            continue;
+
+        queue<int> q;
+
+        q.push(i);
+        gr[i] = 0;
+
+        while (q.empty() == false)
+        {
+            int cur = q.front();
+            q.pop();
+
+            for (int next : adj[cur])
+            {
+                if (gr[next] != -1)
+                {
+                    if (gr[next] == gr[cur])
+                        return false;
+                    else continue;
+                }
+                gr[next] = (gr[cur] + 1) % 2;
+                q.push(next);
+            }
+        }
+    }
+
+    return true;
+}
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
-    cin >> n;
-
-    vector<vector<int>> adj(n + 1);
-
-    int x, y;
-    
-    while (true)
+    cin >> k;
+    while (k--)
     {
-        cin >> x >> y;
+        cin >> v >> e;
 
-        if (x == -1 && y == -1)
-            break;
-
-        adj[x].push_back(y);
-        adj[y].push_back(x);
-    }
-
-    vector<int> points(n + 1, 2147483647);
-
-    for (int i = 1; i <= n; ++i)
-    {
-        queue<int> queue;
-        vector<int> weight(n + 1, -1);
-
-        queue.push(i);
-        weight[i] = 0;
-
-        while (queue.empty() == false)
+        for (int i = 1; i <= v; ++i)
         {
-            int cur = queue.front();
-            queue.pop();
-
-            for (int now : adj[cur])
-            {
-                if (weight[now] == -1)
-                {
-                    weight[now] = weight[cur] + 1;
-                    queue.push(now);
-                }
-            }
+            adj[i].clear();
         }
 
-        points[i] = *max_element(weight.begin(), weight.end());
-    }
+        int i, j;
+        while (e--)
+        {
+            cin >> i >> j;
+            adj[i].push_back(j);
+            adj[j].push_back(i);
+        }
 
-    int r1 = *min_element(points.begin(), points.end());
-
-    vector<int> candidate;
-
-    for (int i = 1; i <= n; ++i)
-    {
-        if (r1 == points[i])
-            candidate.push_back(i);
-    }
-    
-    cout << r1 << " " << (int)candidate.size() << "\n";
-    
-    for (int i : candidate)
-    {
-        cout << i << " ";
+        if (solve() == true)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
     }
 
     return 0;
