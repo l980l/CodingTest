feabf356c560bbcbb0a3507776cce0060c54a037
l980l <kimkwanhyeon98@gmail.com>
2025-03-05 12:16:52 +0900

    BOJ 14267 회사 문화 1

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -4,61 +4,51 @@
 
 using namespace std;
 
+int n, m;
+vector<vector<int>> child;
+vector<int> score;
+
+void dfs(int cur)
+{
+    for (int next : child[cur])
+    {
+        score[next] += score[cur];
+        dfs(next);
+    }
+}
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int N, M;;
+    cin >> n >> m;
 
-    cin >> N >> M;
-
-    // 거리
-    vector<vector<int>> distance(N + 1, vector<int>(N+1, 0));
-    vector<vector<int>> adj(N + 1);
-
-    int u, v, w;
-    for (int i = 1; i < N; ++i)
+    child = vector<vector<int>>(n + 1);
+    score = vector<int>(n + 1, 0);
+    for (int i = 1; i <= n; ++i)
     {
-        cin >> u >> v >> w;
+        int temp;
+        cin >> temp;
 
-        distance[u][v] = w;
-        distance[v][u] = w;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        if (temp == -1)
+            continue;
+
+        child[temp].push_back(i);
     }
 
-    while (M--)
+    int i, w;
+    while (m--)
     {
-        cin >> u >> v;
+        cin >> i >> w;
+        score[i] += w;
+    }
 
-        vector<bool> visit(N + 1);
-        queue<int> q;
-        q.push(u);
-        visit[u] = true;
+    dfs(1);
 
-        while (q.empty() == false)
-        {
-            int cur = q.front();
-            q.pop();
-            
-            if (cur == v)
-            {
-                cout << distance[u][v] << "\n";
-                break;
-            }
-
-            for (int next : adj[cur])
-            {
-                if (visit[next] == true)
-                    continue;
-
-                visit[next] = true;
-                distance[u][next] = distance[u][cur] + distance[cur][next];
-                distance[next][u] = distance[u][cur] + distance[cur][next];
-                q.push(next);
-            }
-        }
+    for (int l = 1; l <= n; ++l)
+    {
+        cout << score[l] << " ";
     }
 
     return 0;
