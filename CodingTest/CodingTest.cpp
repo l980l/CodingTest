8190036d7880080460aee00ae873eb57ff1e52fd
l980l <kimkwanhyeon98@gmail.com>
2025-03-09 11:14:43 +0900

    BOJ 1766 문제집

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -6,73 +6,53 @@
 
 using namespace std;
 
-int n, m;
-string s1, s2;
-
-vector<string> name;
-unordered_map<string, int> id; // 이름을 인덱스로 받으면 사전순 정렬 시의 고유번호를 반환하는 해시
-
-int indeg[1002]; // 각 사람들의 indegree를 셀 배열
-vector<vector<int>> adj; // 인접리스트
-vector<vector<int>> ch; // 각 사람들의 자녀들 고유번호를 저장할 벡터 ch
-
-int main(void) {
+int main(void) 
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    cin >> n;
+    int N, M;
+    cin >> N >> M;
 
-    adj = vector<vector<int>>(n + 1);
-    ch = vector<vector<int>>(n + 1);
-    name = vector<string>(n + 1);
+    vector<int> indegree(N + 1, 0);
+    vector<vector<int>> adj(N + 1);
 
-    for (int i = 1; i <= n; i++)
+    int A, B;
+    while (M--)
     {
-        cin >> name[i];
-    }
-    sort(name.begin(), name.end());
-
-    for (int i = 1; i <= n; i++)
-        id[name[i]] = i;
-
-    cin >> m;
-    for (int i = 0; i < m; i++) 
-    {
-        cin >> s1 >> s2;
-
-        adj[id[s2]].push_back(id[s1]);
-        indeg[id[s1]]++;
+        cin >> A >> B;
+        adj[A].push_back(B);
+        ++indegree[B];
     }
 
-    // 시조들의 고유번호를 저장할 벡터 ac에 indegree가 0인 시조의 고유번호를 기록
-    vector<int> ac;
-    for (int i = 1; i <= n; i++)
-        if (!indeg[i]) ac.push_back(i);
+    vector<int> result;
+    priority_queue<int, vector<int>, greater<int>> pq;
 
-    cout << ac.size() << '\n';
-    for (int a : ac)
-        cout << name[a] << ' ';
-    cout << '\n';
-
-    // indegree가 1 많은 후손(자녀)들의 정보를 배열 ch에 저장
-    for (int i = 1; i <= n; i++) 
+    for (int i = 1; i <= N; ++i)
     {
-        sort(adj[i].begin(), adj[i].end());
-
-        for (int c : adj[i])
-            if (indeg[c] - indeg[i] == 1) 
-                ch[i].push_back(c);
+        if (indegree[i] == 0)
+            pq.push(i);
     }
 
-    // 해당하는 사람과 자식의 수, 그리고 자식들의 이름을 출력
-    for (int i = 1; i <= n; i++) 
+    while (pq.empty() == false)
     {
-        int sz = ch[i].size();
-        cout << name[i] << ' ' << sz << ' ';
+        int cur = pq.top();
+        result.push_back(cur);
+        pq.pop();
 
-        for (int c : ch[i])
-            cout << name[c] << ' ';
-
-        cout << '\n';
+        for (int next : adj[cur])
+        {
+            --indegree[next];
+            
+            if (indegree[next] == 0)
+                pq.push(next);
+        }
     }
+
+    for (int x : result)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
 }
