efbd47b17cc757679ceeb406e071980cdd1b54c7
l980l <kimkwanhyeon98@gmail.com>
2025-03-23 13:09:26 +0900

    BOJ 1786 찾기

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,64 +1,66 @@
 ﻿#include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
-int main(void)
+vector<int> failure(string s)
+{
+    vector<int> f((int)s.size());
+    int j = 0;
+    for (int i = 1; i < (int)s.size(); ++i)
+    {
+        while (j > 0 && s[i] != s[j])
+            j = f[j - 1];
+        if (s[i] == s[j])
+            f[i] = ++j;
+    }
+
+    return f;
+}
+
+int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int N, M, K;
-    cin >> N >> M >> K;
-    vector<vector<pair<long long, int>>> adj(N + 1);
-    vector<long long> d(N + 1, 50000000002);
-    while (M--)
+    string T, P;
+
+    getline(cin, T);
+    getline(cin, P);
+
+    vector<int> f = failure(P);
+    vector<int> F(T.size());      // T와 P로 만든 실패함수 느낌
+
+    int j = 0;
+    for (int i = 0; i < (int)T.size(); ++i)
     {
-        int u, v, c;
-        cin >> u >> v >> c;
-        adj[v].push_back({ c,u });
+        while (j > 0 && T[i] != P[j])
+            j = f[j - 1];
+        if (T[i] == P[j])
+            F[i] = ++j;
+        
+        if (j == P.size())
+            j = f[j - 1];
     }
 
-    vector<int> place;
-    while (K--)
+    int count = 0;
+    vector<int> index;
+    for (int i = 0; i < (int)F.size(); ++i)
     {
-        int p;
-        cin >> p;
-        place.push_back(p);
-        d[p] = 0;
-    }
-
-    priority_queue <pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
-    for (int p : place)
-    {
-        pq.push({ 0, p });
-    }
-
-    while (pq.empty() == false)
-    {
-        long long c = pq.top().first;
-        int cur = pq.top().second;
-        pq.pop();
-
-        if (d[cur] != c)
-            continue;
-
-        for (auto next : adj[cur])
+        if (F[i] == P.size())
         {
-            if (d[next.second] <= c + next.first)
-                continue;
-
-            d[next.second] = c + next.first;
-            pq.push({ d[next.second], next.second });
+            ++count;
+            index.push_back(i - P.size() + 2);     // 문자의 끝 주소이기 때문에 (크기 - 1)만큼 빼준다. 또한 문제에서 원하는 것은 1-indexed이기 때문에 + 1을 한번 더 해준다.
         }
     }
 
-    long long maxCost = *max_element(d.begin() + 1, d.end());
-    int index = max_element(d.begin() + 1, d.end()) - d.begin();
-    cout << index << "\n";
-    cout << maxCost;
+    cout << count << '\n';
+    for (int i : index)
+    {
+        cout << i << ' ';   
+    }
 
     return 0;
 }
