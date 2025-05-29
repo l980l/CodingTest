8fd4c024aed11eebec35bb435c87eb4f4473c4d9
l980l <kimkwanhyeon98@gmail.com>
2025-05-29 16:41:56 +0900

    BOJ 2110 공유기 설치

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -4,42 +4,51 @@
 
 using namespace std;
 
-int main() 
+int n, c, x;
+vector<int> h;
+
+bool func(int step)
+{
+    int idx = 0;
+    int count = 0;
+    
+    while (idx < n)
+    {
+        idx = lower_bound(h.begin() + idx, h.end(), (h.front() + step)) - h.begin();
+        ++count;
+    }
+
+    return count >= c;
+}
+
+int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int T, x, y;
-
-    cin >> T;
-
-    while (T--)
+    cin >> n >> c;
+    for (int i = 0; i < n; ++i)
     {
-        cin >> x >> y;
-
-        x = y - x;
-
-        int result = 0;
-        int remain = x;
-        int stepNum = 1;
-        while (remain > 0)
-        {
-            remain -= stepNum * 2;
-            result += 2;
-
-            if (remain <= 0)
-            {
-                if (remain + stepNum <= 0)
-                    result--;
-
-                break;
-            }
-
-            stepNum++;
-        }
-        
-        cout << result << '\n';
+        cin >> x;
+        h.push_back(x);
     }
+
+    sort(h.begin(), h.end());
+
+    int st = 0;
+    int en = h.back();
+
+    while (st < en)
+    {
+        int mid = (st + en + 1) / 2;
+
+        if (func(mid))
+            st = mid;
+        else
+            en = mid - 1;
+    }
+
+    cout << st;
 
     return 0;
 }
