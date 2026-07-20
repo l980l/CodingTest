7681817d32f86a4c57c9a507b6fe0cf61b3834b4
l980l <kimkwanhyeon98@gmail.com>
2026-07-20 23:01:01 +0900

    swea 4008

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -4,51 +4,63 @@
 
 using namespace std;
 
-#define X first
-#define Y second
-
-int t, n, l;
-vector<pair<int, int>> food;
-int result;
-
-void func(int sumT, int sumK, int st)
-{
-    if (sumT > result)
-    {
-        result = sumT;
-    }
-
-    for (int i = st; i < n;++i)
-    {
-        if (sumK + food[i].Y <= l)
-            func(sumT + food[i].X, sumK + food[i].Y, i + 1);
-    }
-}
-
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
+    int t, n;
     cin >> t;
 
-    for (int tcn = 1;tcn <= t;++tcn)
+    for (int tcn = 1; tcn <= t; ++tcn)
     {
-        cin >> n >> l;
-        
-        result = 0;
-        food.clear();
-        
-        for (int i = 0;i < n;++i)
+        cin >> n;
+        vector<int> nums(n);
+        vector<int> ops;
+
+        for (int i = 0; i < 4; ++i)
         {
-            int x, y;
-            cin >> x >> y;
-            food.push_back({ x,y });
+            int temp;
+            cin >> temp;
+            for (int j = 0; j < temp; ++j)
+            {
+                ops.push_back(i);
+            }
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> nums[i];
         }
 
-        func(0, 0, 0);
+        int minS = 0x3f3f3f3f;
+        int maxS = -0x3f3f3f3f;
+        do
+        {
+            int curSum = nums[0];
+            for (int i = 0; i < n - 1; ++i)
+            {
+                switch (ops[i])
+                {
+                case 0:
+                    curSum += nums[i + 1];
+                    break;
+                case 1:
+                    curSum -= nums[i + 1];
+                    break;
+                case 2:
+                    curSum *= nums[i + 1];
+                    break;
+                case 3:
+                    curSum /= nums[i + 1];
+                    break;
+                }
+            }
 
-        cout << "#" << tcn << " " << result << "\n";
+            minS = min(minS, curSum);
+            maxS = max(maxS, curSum);
+        } while (next_permutation(ops.begin(), ops.end()));
+
+        cout << "#" << tcn << " " << maxS - minS << "\n";
     }
 
     return 0;
