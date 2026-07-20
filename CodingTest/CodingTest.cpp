f5082feca67e05ddc2fc37ae508a88df64196a04
l980l <kimkwanhyeon98@gmail.com>
2026-07-20 23:43:41 +0900

    swea 1861

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,67 +1,90 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
+int dx[] = { 1,0,-1,0 };
+int dy[] = { 0,-1,0,1 };
+
+#define X first
+#define Y second
+
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int t, n;
-    cin >> t;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    for (int tcn = 1; tcn <= t; ++tcn)
-    {
-        cin >> n;
-        vector<int> nums(n);
-        vector<int> ops;
+	int t, n;
+	cin >> t;
 
-        for (int i = 0; i < 4; ++i)
-        {
-            int temp;
-            cin >> temp;
-            for (int j = 0; j < temp; ++j)
-            {
-                ops.push_back(i);
-            }
-        }
-        for (int i = 0; i < n; ++i)
-        {
-            cin >> nums[i];
-        }
+	for (int tcn = 1; tcn <= t; ++tcn)
+	{
+		cin >> n;
+		vector<vector<int>> board(n, vector<int>(n));
+		vector<vector<bool>> visit(n,vector<bool>(n));
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = 0; j < n; ++j)
+			{
+				cin >> board[i][j];
+			}
+		}
 
-        int minS = 0x3f3f3f3f;
-        int maxS = -0x3f3f3f3f;
-        do
-        {
-            int curSum = nums[0];
-            for (int i = 0; i < n - 1; ++i)
-            {
-                switch (ops[i])
-                {
-                case 0:
-                    curSum += nums[i + 1];
-                    break;
-                case 1:
-                    curSum -= nums[i + 1];
-                    break;
-                case 2:
-                    curSum *= nums[i + 1];
-                    break;
-                case 3:
-                    curSum /= nums[i + 1];
-                    break;
-                }
-            }
+		int maxSize = 0;
+		int roomNum = 0;
+		queue<pair<int, int>> q;
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = 0; j < n; ++j)
+			{
+				if (visit[i][j])
+					continue;
+				visit[i][j] = true;
+				q.push({ i, j });
+				int curSize = 1;
+				int curRN = board[i][j];
 
-            minS = min(minS, curSum);
-            maxS = max(maxS, curSum);
-        } while (next_permutation(ops.begin(), ops.end()));
+				while (!q.empty())
+				{
+					auto p = q.front();
+					int rn = board[p.X][p.Y];
+					q.pop();
 
-        cout << "#" << tcn << " " << maxS - minS << "\n";
-    }
+					for (int dir = 0; dir < 4; ++dir)
+					{
+						int nx = p.X + dx[dir];
+						int ny = p.Y + dy[dir];
 
-    return 0;
+						if (nx < 0 || nx >= n || ny < 0 || ny >= n || visit[nx][ny])
+							continue;
+						if (board[nx][ny] + 1 != rn && board[nx][ny] - 1 != rn)
+							continue;
+						
+						if (board[nx][ny] + 1 == rn)
+							curRN = rn - 1;
+
+						++curSize;
+						visit[nx][ny] = true;
+						q.push({ nx,ny });
+					}
+				}
+
+				if (maxSize < curSize)
+				{
+					maxSize = curSize;
+					roomNum = curRN;
+				}
+				else if (maxSize == curSize && roomNum > curRN)
+				{
+					roomNum = curRN;
+				}
+			}
+		}
+
+		cout << "#" << tcn << " " << roomNum << " " << maxSize << "\n";
+	}
+
+	return 0;
 }
