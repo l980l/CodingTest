f05f035a4bbc6ff384ba1737761b7c043ad527a7
l980l <kimkwanhyeon98@gmail.com>
2026-07-26 00:03:49 +0900

    swea 1979

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -9,28 +9,69 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int t;
+    int t, n, k;
     cin >> t;
 
-    for (int tcn = 1;tcn <= t;++tcn)
+    for (int tcn = 1; tcn <= t;++tcn)
     {
-        p.clear();
-        string temp;
-        cin >> temp;
+        cin >> n >> k;
+
+        vector<vector<int>> board(n, vector<int>(n));
+        vector<vector<bool>> visitR(n, vector<bool>(n));
+        vector<vector<bool>> visitC(n, vector<bool>(n));
+        for (int i = 0;i < n;++i)
+        {
+            for (int j = 0; j < n;++j)
+            {
+                cin >> board[i][j];
+            }
+        }
 
         int result = 0;
-        int sum = temp[0] - '0';
-        for (int i = 1; i < temp.size();++i)
+        for (int i = 0; i < n; ++i)
         {
-            int cur = temp[i] - '0';
-
-            if (cur > 0 && i > sum)
+            for (int j = 0; j < n;++j)
             {
-                result += i - sum;
-                sum += i - sum;
-            }
+                if (board[i][j] == 0)
+                    continue;
 
-            sum += cur;
+                if (!visitR[i][j])
+                {
+                    visitR[i][j] = true;
+
+                    int curLine = 1;
+                    int nr = i;
+
+                    while (nr + 1 < n && board[nr + 1][j] == 1)
+                    {
+                        ++nr;
+                        visitR[nr][j] = true;
+
+                        ++curLine;
+                    }
+
+                    if (curLine == k)
+                        ++result;
+                }
+
+                if (!visitC[i][j])
+                {
+                    visitC[i][j] = true;
+
+                    int curLine = 1;
+                    int nc = j;
+                    while (nc + 1 < n && board[i][nc + 1] == 1)
+                    {
+                        ++nc;
+                        visitC[i][nc] = true;
+
+                        ++curLine;
+                    }
+
+                    if (curLine == k)
+                        ++result;
+                }
+            }
         }
 
         cout << "#" << tcn << " " << result << "\n";
